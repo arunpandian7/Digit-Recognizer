009831ca2447cae5f88aba1434d718f841637622
Arun Pandian R <smartboyarun56@gmail.com>
2020-03-31 22:59:49 +0530

    server files created

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server started at port 3000");
+});
+ 
